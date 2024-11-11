2f9742da5e446d9df6963c14f77bf54f396fbd27
zurii11 <zukaa.aleksaia@gmail.com>
2024-11-11 16:46:37 +0400

    Added support for multiple concurrent connections

--- server.c
+++ server.c
@@ -1,11 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+
+int read_socket(int sfd, char *buffer)
+{
+  ssize_t read_length = read(sfd, buffer, 1024);
+  if(read_length < 0) {
+    printf("Failed to read from a connection...\n");
+    return 1;
+  }
+
+  char path[100];
+  int path_len = 1;
+  int path_start = 3;
+
+  char *user_agent_header = strtok(buffer, "\r\n");
+  char *user_agent_value;
+
+  while(user_agent_header != NULL) {
+    if(strncmp(user_agent_header, "User-Agent", 10) == 0)
+      break;
+
+    user_agent_header = strtok(NULL, "\r\n");
+  }
+
+  if(user_agent_header != NULL) {
+    user_agent_value = strtok(user_agent_header, " ");
+    user_agent_value = strtok(NULL, " ");
+  }
+
+  while(buffer[path_start+path_len] != ' ') {
+    path[path_len-1] = buffer[path_start+path_len];
+    path_len++;
+  }
+
+  path[path_len-1] = '\0';
+  if(strcmp(path, "/") == 0) {
+    char response[1024] = {0};
+    char headers[128] = "Content-Type: text/plain\r\n";
+    char content_length[64];
+    char body[10] = {0};
+    sprintf(content_length, "Content-Length: %d\r\n\r\n", 4);
+    strcat(headers, content_length);
+    strcat(body, "Test\n");
+    strcat(body, "\r\n\r\n");
+    if(createResponse(200, headers, body, response) < 0)
+      printf("NULL\n");
+    printf("RESPONSE: %s\n", response);
+    if(write(sfd, response, strlen(response)) < 0)  {
+      printf("Failed to send a response...\n");
+      return 1;
+    }  
+  }
+  else if(strncmp(path, "/user-agent", 11) == 0) {
+    char response[1024] = {0};
+    char headers[128] = "Content-Type: text/plain\r\n";
+    char content_length[64];
+    char body[sizeof(user_agent_value) + 4];
+    sprintf(content_length, "Content-Length: %zu\r\n\r\n", strlen(user_agent_value));
+    strcat(headers, content_length);
+    strcpy(body, user_agent_value);
+    strcat(body, "\r\n\r\n");
+
+    if(createResponse(200, headers, body, response) < 0)
+      printf("NULL\n");
+    printf("RESPONSE: %s\n", response);
+    if(write(sfd, response, strlen(response)) < 0) {
+      printf("Failed to send a response...\n");
+      return 1;
+    }
+  }
+  else if(strncmp(path, "/echo", 5) == 0) {
+    char substr[128];
+    char headers[128] = "Content-Type: text/plain\r\n";
+    char content_length[64];
+    sprintf(content_length, "Content-Length: %d\r\n\r\n", path_len);
+    strcat(headers, content_length);
+    char body[sizeof(path)+5];
+    strncpy(substr, path+5, path_len-5);
+    strcpy(body, substr);
+    strcat(body, "\r\n\r\n");
+
+    char response[1024] = {0};
+    if(createResponse(200, headers, body, response) < 0)
+      printf("NULL\n");
+    if(write(sfd, response, strlen(response)) < 0) {
+      printf("Failed to send a response...\n");
+      return 1;
+    }
+  } 
+  else {
+    char response[1024] = {0};
+    printf("%s\n", path);
+    createResponse(404, NULL, NULL, response);
+    if(write(sfd, response, strlen(response)) < 0) {
+      printf("Failed to send a response...\n");
+      return 1;
+    }
+  }
+
+  return 0;
+}
 
 int createResponse(int response_code, char* headers, char* body, char* buffer)
 {
@@ -32,141 +133,86 @@ int createResponse(int response_code, char* headers, char* body, char* buffer)
 }
 
 int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
+  // Disable output buffering
+  setbuf(stdout, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  printf("Logs from your program will appear here!\n");
 
-  int server_fd, client_addr_len, new_socket;
-  ssize_t read_length;
+  int server_fd, client_addr_len;
+  fd_set read_fds, active_fds;
   char buffer[1024];
   struct sockaddr_in client_addr;
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1) {
+    printf("Socket creation failed: %s...\n", strerror(errno));
+    return 1;
+  }
 
-	// Since the tester restarts your program quite often, setting REUSE_PORT
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-		return 1;
-	}
+  // Since the tester restarts your program quite often, setting REUSE_PORT
+  // ensures that we don't run into 'Address already in use' errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+    printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+    return 1;
+  }
 
-	struct sockaddr_in serv_addr = { 
+  struct sockaddr_in serv_addr = { 
     .sin_family = AF_INET,
     .sin_port = htons(4221),
     .sin_addr = { htonl(INADDR_ANY) },
   };
 
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
-
-	new_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
-
-  read_length = read(new_socket, buffer, 1024);
-  if(read_length < 0) {
-    printf("Failed to read from a connection...\n");
+  if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+    printf("Bind failed: %s \n", strerror(errno));
     return 1;
   }
 
-  char path[100];
-  int path_len = 1;
-  int path_start = 3;
-
-  char *user_agent_header = strtok(buffer, "\r\n");
-  char *user_agent_value;
-
-  while(user_agent_header != NULL) {
-    if(strncmp(user_agent_header, "User-Agent", 10) == 0)
-      break;
-
-    user_agent_header = strtok(NULL, "\r\n");
-  }
-  if(user_agent_header != NULL) {
-    user_agent_value = strtok(user_agent_header, " ");
-
-    user_agent_value = strtok(NULL, " ");
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    printf("Listen failed: %s \n", strerror(errno));
+    return 1;
   }
 
+  printf("Waiting for a client to connect...\n");
+  client_addr_len = sizeof(client_addr);
 
-  while(buffer[path_start+path_len] != ' ') {
-    path[path_len-1] = buffer[path_start+path_len];
-    path_len++;
-  }
-  path[path_len-1] = '\0';
+  // Initialize fd sets
+  FD_ZERO(&active_fds);
+  FD_SET(server_fd, &active_fds);
 
-  if(strcmp(path, "/") == 0) {
-    char response[1024] = {0};
-    if(createResponse(200, NULL, NULL, response) < 0)
-      printf("NULL\n");
-    printf("RESPONSE: %s\n", response);
-    if(write(new_socket, response, strlen(response)) < 0)  {
-      printf("Failed to send a response...\n");
-      return 1;
-    }  
-  }
-  else if(strncmp(path, "/user-agent", 11) == 0) {
-    char response[1024] = {0};
-    char headers[128] = "Content-Type: text/plain\r\n";
-    char content_length[64];
-    char body[sizeof(user_agent_value) + 4];
-    sprintf(content_length, "Content-Length: %zu\r\n\r\n", strlen(user_agent_value));
-    strcat(headers, content_length);
-    strcpy(body, user_agent_value);
-    strcat(body, "\r\n\r\n");
+  while(1) {
+    read_fds = active_fds;
 
-    if(createResponse(200, headers, body, response) < 0)
-      printf("NULL\n");
-    if(write(new_socket, response, strlen(response)) < 0) {
-      printf("Failed to send a response...\n");
+    if(select(FD_SETSIZE, &read_fds, NULL, NULL, NULL) < 0){
+      printf("Select failed: %s \n", strerror(errno));
       return 1;
     }
-  }
-  else if(strncmp(path, "/echo", 5) == 0) {
-    char substr[128];
-    char headers[128] = "Content-Type: text/plain\r\n";
-    char content_length[64];
-    sprintf(content_length, "Content-Length: %d\r\n\r\n", path_len);
-    strcat(headers, content_length);
-    char body[sizeof(path)+5];
-    strncpy(substr, path+5, path_len-5);
-    strcpy(body, substr);
-    printf("BODY: %s\n", body);
-    char response[1024] = {0};
-    createResponse(200, headers, body, response);
-    if(write(new_socket, response, strlen(response)) < 0) {
-      printf("Failed to send a response...\n");
-      return 1;
-    }
-  } 
-  else {
-    char response[1024] = {0};
-    printf("%s\n", path);
-    createResponse(404, NULL, NULL, response);
-    if(write(new_socket, response, strlen(response)) < 0) {
-      printf("Failed to send a response...\n");
-      return 1;
+
+    for(int i = 0; i < FD_SETSIZE; i++) {
+      if(FD_ISSET(i, &read_fds)) {
+        // Server socket available for read operation, means there is a connection request on that socket
+        if(i == server_fd) {
+          int new_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+          if(new_socket < 0) {
+            printf("Error accepting a connection: %s\n", strerror(errno));
+            return 1;
+          }
+          printf("Client connected: %d\n", inet_ntoa(client_addr.sin_addr));
+
+          FD_SET(new_socket, &active_fds);
+        } else {
+          if(read_socket(i, buffer) != 0) {
+            close(i);
+            FD_CLR(i, &active_fds);
+          }
+        }
+      }
     }
   }
 
-	close(server_fd);
+  close(server_fd);
 
-	return 0;
+  return 0;
 }
